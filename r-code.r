b699d88efe33f1c1beaf69b4709da61f7c0adeab
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2023-10-31 11:38:31 +0100

    first draft of solution

--- r-code.r
+++ r-code.r
@@ -1,3 +1,6 @@
+# Install libraries
+require(AER)
+
 # BEGIN: yzqjx8d02wtr
 # Read the finlit.csv file
 setwd("/Users/sinfjell/Library/CloudStorage/OneDrive-NorgesHandelshøyskole/FIE401/assignment-3")
@@ -48,4 +51,42 @@ linear_model_1st_stage <- lm(adv.lit.index ~ sibling_worse + sibling_better + pa
 # Summary of the model to get coefficients and other statistics
 summary(linear_model_1st_stage)
 
-# END: yzqjx8d02wtr
+fit2 <- ivreg(mkt.part ~ adv.lit.index + AGE_30_40 + AGE_40_50 + AGE_50_60 + AGE_OVER_60 +
+    edu3 + edu4 + edu5 + edu6 
+    + male + partner + numkids + retired + selfempl + lincome + 
+    wq2 + wq3 + wq4 | sibling_worse + sibling_better + parent_intermediate_high + parent_dont_know + 
+    AGE_30_40 + AGE_40_50 + AGE_50_60 + AGE_OVER_60 +
+    edu3 + edu4 + edu5 + edu6 
+    + male + partner + numkids + retired + selfempl + lincome + 
+    wq2 + wq3 + wq4, data=finlit)
+
+summary(fit2)
+
+# Load stargazer library
+library(stargazer)
+
+# Print stargazer for linear_model, linear_model_1st_stage, and fit2
+stargazer(linear_model, linear_model_1st_stage, fit2, type = "text", 
+dep.var.labels = c("Standard OLS", "First stage IV", "Second stage IV in one go"))
+
+# ----------- exogeneity
+# First checking for relevance 
+# if F is under 10 we have weak instrumental variables that explain little of the variation in the endogenous variable
+#Define H0
+myH0 <- c("sibling_worse = 0", "sibling_better = 0", "parent_dont_know = 0", "parent_intermediate_high = 0")
+#F-statistic 
+linearHypothesis(linear_model_1st_stage, myH0) # over 15, good
+
+finlit$res <- residuals(fit2)
+
+fit_test <- lm(res ~ sibling_worse + sibling_better + parent_intermediate_high + parent_dont_know + 
+    AGE_30_40 + AGE_40_50 + AGE_50_60 + AGE_OVER_60 +
+    edu3 + edu4 + edu5 + edu6 
+    + male + partner + numkids + retired + selfempl + lincome + 
+    wq2 + wq3 + wq4, data = finlit)
+
+Fstat <- linearHypothesis(fit_test, myH0)$F[2] # F-statistic
+m <- 4 # number of instruments
+k <- 1 # number of endogenous variables
+testval <- m * Fstat # test statistic
+pval <- 1 - pchisq(testval, (m - k)) # p-value

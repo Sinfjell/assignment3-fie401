f0ac9a75e00f4c0977ec77922b2387f56d84c4fc
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2023-10-31 10:37:55 +0100

    interim push

--- r-code.r
+++ r-code.r
@@ -1,14 +1,52 @@
 # BEGIN: yzqjx8d02wtr
 # Read the finlit.csv file
+setwd("/Users/sinfjell/Library/CloudStorage/OneDrive-NorgesHandelshøyskole/FIE401/assignment-3")
 finlit <- read.csv("finlit.csv")
 
 # View the first few rows of the data
 head(finlit)
 
+# Create binary age variables
+finlit$AGE_UNDER_30 <- as.integer(finlit$age == "<30")
+finlit$AGE_30_40 <- as.integer(finlit$age %in% c("<30", "30s"))
+finlit$AGE_40_50 <- as.integer(finlit$age == "40s")
+finlit$AGE_50_60 <- as.integer(finlit$age == "50s")
+finlit$AGE_OVER_60 <- as.integer(finlit$age == ">60")
+
+# Create a new dataframe with age binary variables and age variable
+age_df <- data.frame(
+    AGE_UNDER_30 = finlit$AGE_UNDER_30,
+    AGE_30_40 = finlit$AGE_30_40,
+    AGE_40_50 = finlit$AGE_40_50,
+    AGE_50_60 = finlit$AGE_50_60,
+    AGE_OVER_60 = finlit$AGE_OVER_60,
+    age = finlit$age
+)
+
+finlit$nonequity.wealth.cat
+# Create binary variables for nonequity.wealth.cat
+finlit$wq2 <- as.integer(finlit$nonequity.wealth.cat == 2)
+finlit$wq3 <- as.integer(finlit$nonequity.wealth.cat == 3)
+finlit$wq4 <- as.integer(finlit$nonequity.wealth.cat == 4)
+
+age_df_2 <- data.frame(
+    non_equity = finlit$nonequity.wealth.cat,
+    AGE_30_40 = finlit$wq2,
+    AGE_40_50 = finlit$wq3,
+    AGE_50_60 = finlit$wq4
+)
+
+# View the first few rows of the data with binary age variables
+head(finlit)
+
+# View the first few rows of the data with age dummies
+head(finlit)
+
 # Fitting a Linear Probability Model
-linear_model <- lm(mkt.part ~ adv.lit.index + 
-    edu2 + edu3 + edu4 + edu5 + edu6 
-    + male + partner + numkids + retired + selfempl + lincome + nonequity.wealth.cat, data=finlit)
+linear_model <- lm(mkt.part ~ adv.lit.index + AGE_UNDER_30 + AGE_30_40 + AGE_40_50 + AGE_50_60 + AGE_OVER_60 +
+    edu3 + edu4 + edu5 + edu6 
+    + male + partner + numkids + retired + selfempl + lincome + 
+    wq2 + wq3 + wq4, data=finlit)
 
 # Summary of the model to get coefficients and other statistics
 summary(linear_model)
@@ -34,15 +72,9 @@ library(stargazer)
 models_list <- list(linear_model, first_stage, second_stage)
 
 # Use stargazer to print the summaries side by side
-# Rename variables for more understandable names in printout
-names(linear_model)[names(linear_model) == "adv.lit.index"] <- "Market Participation"
-names(first_stage)[names(first_stage) == "adv.lit.index"] <- "Predicted Adult Literacy Index"
-
 # Print summaries with stargazer
 stargazer(linear_model, first_stage, second_stage, type = "text", 
           title = "Summary of Linear Probability Model and First Stage Regression",
           align = TRUE
           #dep.var.labels = c("Market Participation", "Predicted Adult Literacy Index", "Market Participation")
          )
-
-
